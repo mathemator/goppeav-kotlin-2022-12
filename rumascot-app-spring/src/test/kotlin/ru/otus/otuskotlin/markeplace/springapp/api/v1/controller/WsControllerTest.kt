4ae62c497828a36416c4e745ad5f56eb95de7a57
mathemator <mathemator@yandex.ru>
2023-06-13 09:48:30 +0300

    m3 - websocket

---
+++ rumascot-app-spring/src/test/kotlin/ru/otus/otuskotlin/markeplace/springapp/api/v1/controller/WsControllerTest.kt
@@ -0,0 +1,112 @@
+package ru.otus.goppeav.rumascot.springapp.api.v1.controller
+
+import io.kotest.common.runBlocking
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import ru.otus.goppeav.rumascot.api.v1.apiV1Mapper
+import ru.otus.goppeav.rumascot.api.v1.models.*
+import java.net.URI
+import javax.websocket.*
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class WsControllerTest {
+
+    @LocalServerPort
+    private var port: Int = 0
+
+    private lateinit var container: WebSocketContainer
+    private lateinit var client: TestWebSocketClient
+
+    @BeforeEach
+    fun setup() {
+        container = ContainerProvider.getWebSocketContainer()
+        client = TestWebSocketClient()
+    }
+
+    @Test
+    fun initSession() {
+        runBlocking {
+            withContext(Dispatchers.IO) {
+                container.connectToServer(client, URI.create("ws://localhost:${port}/ws/v1"))
+            }
+
+            withTimeout(3000) {
+                while (client.session?.isOpen != true) {
+                    delay(100)
+                }
+            }
+            assert(client.session?.isOpen == true)
+            withTimeout(3000) {
+                val response = client.receive()
+                val message = apiV1Mapper.readValue(response, IResponse::class.java)
+                assert(message is AdInitResponse)
+                assert(message.result == ResponseResult.SUCCESS)
+            }
+        }
+    }
+
+    @Test
+    fun response() {
+        runBlocking {
+            withContext(Dispatchers.IO) {
+                container.connectToServer(client, URI.create("ws://localhost:${port}/ws/v1"))
+            }
+
+            withTimeout(3000) {
+                while (client.session?.isOpen != true) {
+                    delay(100)
+                }
+            }
+            assert(client.session?.isOpen == true)
+
+            withTimeout(30000) {
+                client.receive() // игнорируем инит
+                delay(200)
+                val request = apiV1Mapper.writeValueAsString(AdReadRequest(requestId = "1234", requestType = "read"))
+                client.session?.basicRemote?.sendText(request)
+
+                val response = client.receive()
+                val responseMessage = apiV1Mapper.readValue(response, IResponse::class.java)
+                assert(responseMessage is AdReadResponse)
+                assert(responseMessage.result == ResponseResult.SUCCESS)
+            }
+        }
+    }
+}
+
+@ClientEndpoint
+class TestWebSocketClient {
+    var session: Session? = null
+    private val messages: MutableList<String> = mutableListOf()
+
+    @OnOpen
+    fun onOpen(session: Session?) {
+        this.session = session
+    }
+
+    @OnClose
+    fun onClose(session: Session?, reason: CloseReason) {
+        println("Session is closed due to ${reason.reasonPhrase}")
+        this.session = null
+    }
+
+    @OnMessage
+    fun onMessage(message: String) {
+        println("Received text message: $message")
+        messages.add(message)
+    }
+
+    suspend fun receive(): String {
+        while (messages.isEmpty()) {
+            delay(100)
+        }
+        return messages.removeFirst()
+    }
+}
